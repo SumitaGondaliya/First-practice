310c7dfdd99bd3c349f3b2371bf36d502ca6e50b
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-26 23:42:16 +0530

    set1sol5

--- Very Easy/Set-1/Problem5.js
+++ Very Easy/Set-1/Problem5.js
@@ -6,3 +6,11 @@
     Notes :
        - The first element in an array has an index of 0.
  */
+
+function array(arr) {
+  let firstelement = arr[0];
+  return firstelement;
+}
+const first = [1, 2, 3, 4, 5, 6];
+let ans = array(first);
+console.log("First element is :", ans);
