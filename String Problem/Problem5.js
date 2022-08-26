1daaea9926070c98ee34044c9bd1e187a6221936
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-26 15:24:54 +0530

    Solution5

--- String Problem/Problem5.js
+++ String Problem/Problem5.js
@@ -15,3 +15,21 @@
         but don't copy paste code from google search.
             */
 }
+
+/*function Stringlength(string) {
+  const char = string.length;
+  return char;
+}
+let count = Stringlength("Hello World 123");
+console.log("String length is:", count);*/
+
+const string = "Hello World 12345";
+let Acount = (string.match(/[A-za-z]/g), string.length);
+console.log("Alphabates :", Acount);
+
+let Dcount = (string.replace(/[^0-9]/g,'').length);
+console.log("Digits :", Dcount);
+
+const Sstring="Hello World _*%$#@!";
+var Scount=(Sstring.match(/[@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g))
+console.log(" Special character:", Scount.length);
