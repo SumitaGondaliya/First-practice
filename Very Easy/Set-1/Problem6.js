1d132e372dc956d361f86ae2fc83934cd83ed7c3
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-27 14:17:26 +0530

    Set1sol6

--- Very Easy/Set-1/Problem6.js
+++ Very Easy/Set-1/Problem6.js
@@ -4,3 +4,13 @@
  * Examples => circuitPower(230, 10) ➞ 2300
  * Hint => P = V * I  (power equals voltage times current)
  */
+
+
+function circuitPower(voltage,current){
+
+    let power=voltage*current
+    return power
+}
+
+let calculatedpower=circuitPower(230,10)
+console.log("Calucalated Power is :",calculatedpower)
