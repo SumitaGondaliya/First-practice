dcbf1c9561e1a313c2c6595dc178a3df3b2fb605
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-27 16:14:34 +0530

    Set2sol10

--- Very Easy/Set-2/Problem10.js
+++ Very Easy/Set-2/Problem10.js
@@ -1,5 +1,6 @@
 /**
-    Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far.
+    Create a function that takes the number of wins, draws and losses and calculates the number of points a football
+     team has obtained so far.
 
         - wins get 3 points
         - draws get 1 point
@@ -13,3 +14,17 @@
     Notes =>
         - Inputs will be numbers greater than or equal to 0.
  */
+
+/*  3*3       9    5*3   15
+    4*1       4    0*1    0
+    2*0       0    2*0    0*/
+
+function footballPoints(wins, draws, losses) {
+  const cal = (wins * 3) + (draws * 1 )+ (losses * 0)
+  /*const cal1 = draws * 1;
+  const cal2 = losses * 0;*/
+  return cal;
+}
+
+let points = footballPoints(5,0,2);
+console.log("Football team has obtained :", points, "points");
