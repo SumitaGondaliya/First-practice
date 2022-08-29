4f042273653b6677e6b2a6a550e89daa2721b447
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-30 00:47:09 +0530

    set3sol3

--- Very Easy/Set-3/Problem3.js
+++ Very Easy/Set-3/Problem3.js
@@ -1,8 +1,16 @@
 /**
-     Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; otherwise return false.
+     Create a function that takes three arguments prob, prize, pay and returns true if prob * prize > pay; 
+     otherwise return false.
 
      Example =>
           profitableGamble(0.2, 50, 9) ➞ true
           profitableGamble(0.9, 1, 2) ➞ false
           profitableGamble(0.9, 3, 2) ➞ true
  */
+
+function profitableGamble(prob, prize, pay) {
+  return prob * prize > pay ? "true" : "False";
+}
+
+let result = profitableGamble(0.9, 3, 2);
+console.log("Gamble progit value is :", result);
