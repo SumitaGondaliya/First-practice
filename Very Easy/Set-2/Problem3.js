ff13cd35464140b95c21840a0d61d79632ff4e27
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-29 22:36:15 +0530

    set2sol3

--- Very Easy/Set-2/Problem3.js
+++ Very Easy/Set-2/Problem3.js
@@ -10,3 +10,11 @@
       - n will always be greater than 2.
       - The formula (n - 2) x 180 gives the sum of all the measures of the angles of an n-sided polygon.
  */
+
+function sumPolygon(n) {
+  let sum = (n - 2) * 180;
+  return sum;
+}
+
+let result = sumPolygon(3);
+console.log("Total sum of internal angles:", result);
