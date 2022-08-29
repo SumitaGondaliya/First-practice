0a09d6f5d1d8cd70525c0ac59eca0200b42ca33b
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-29 16:27:37 +0530

    set2sol5

--- Very Easy/Set-2/Problem5.js
+++ Very Easy/Set-2/Problem5.js
@@ -1,22 +1,20 @@
 /**
- * A student learning JavaScript was trying to make a function. His code should 
- * concatenate a passed string name with string "Edabit" and store it in a variable called result. 
+ * A student learning JavaScript was trying to make a function. His code should
+ * concatenate a passed string name with string "Edabit" and store it in a variable called result.
  * He needs your help to fix this code.
  *
  * Examples =>
  *    nameString("Mubashir") ➞ "MubashirEdabit"
  *    nameString("Matt") ➞ "MattEdabit"
  *    nameString("javaScript") ➞ "javaScriptEdabit"
- * 
+ *
  *
  */
 
-
-function nameString(str){
-    let string=str.concat("Edabit")
-    return string
-
+function nameString(str) {
+  let string = str.concat("Edabit");
+  return string;
 }
 
-let result=nameString("javaScript")
-console.log("student's concatenated string is:",result)
+let result = nameString("javaScript");
+console.log("student's concatenated string is:", result);
