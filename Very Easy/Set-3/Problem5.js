96f83b0c766a8029dc28db243ad5edf7e5be5ed4
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-30 10:42:49 +0530

    set3Sol5

--- Very Easy/Set-3/Problem5.js
+++ Very Easy/Set-3/Problem5.js
@@ -1,5 +1,6 @@
 /**
-    Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10.
+    Create a function that takes two arguments. Both arguments are integers, a and b.
+     Return true if one of them is 10 or if their sum is 10.
 
     Examples =>
         makesTen(9, 10) ➞ true
@@ -8,3 +9,10 @@
     
 
  */
+function makesTen(a, b) {
+  let arg = a + b == 10 || a == 10 || b == 10;
+  return arg;
+}
+
+let answer = makesTen(1,9);
+console.log("given argument is :", answer);
