10399f5bfc97e09b9db641c1cfc60634ee125b87
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-30 11:34:59 +0530

    set3Sol9

--- Very Easy/Set-3/Problem9.js
+++ Very Easy/Set-3/Problem9.js
@@ -9,3 +9,10 @@
    Notes =>
       - Don't forget to return the result.
  */
+
+function divisibleByFive(num) {
+  return num % 5 === 0 ? "True" : "False";
+}
+
+let result = divisibleByFive(-55);
+console.log("Value is divisble by 5 and", result);
