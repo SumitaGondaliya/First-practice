1e1937ebb0dca0307d39bd16a43f946bf2908223
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 18:59:54 +0530

    set4Sol8

--- Very Easy/Set-4/Problem8.js
+++ Very Easy/Set-4/Problem8.js
@@ -1,7 +1,9 @@
 /**
-   A bartender is writing a simple program to determine whether he should serve drinks to someone. He only serves drinks to people 18 and older and when he's not on break.
+   A bartender is writing a simple program to determine whether he should serve drinks to someone. 
+   He only serves drinks to people 18 and older and when he's not on break.
 
-   Given the person's age, and whether break time is in session, create a function which returns whether he should serve drinks.
+   Given the person's age, and whether break time is in session, 
+   create a function which returns whether he should serve drinks.
 
    Examples =>
       shouldServeDrinks(17, true) ➞ false
@@ -12,3 +14,10 @@
       - Return true or false.
 
  */
+
+      function shouldServeDrinks(age, onBreak) 
+      {
+         return age >= 18 && onBreak == false ? "true" : "false" 
+      }       
+      let result=shouldServeDrinks(19,false)
+      console.log("he should serve drinks to this person",result)
