afd94cbec6efe7d1cfa82ca749fd27e062b899b8
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 21:18:24 +0530

    set5Sol9

--- Very Easy/Set-5/Problem9.js
+++ Very Easy/Set-5/Problem9.js
@@ -1,7 +1,8 @@
 /**
    ---- Many Operators! ----
 
-   Some basic arithmetic operators are +, -, *, /, and %. In this challenge you will be given three parameters, num1, num2, and an operator. Use the operator on number 1 and 2.
+   Some basic arithmetic operators are +, -, *, /, and %. In this challenge you will be given three parameters,
+   num1, num2, and an operator. Use the operator on number 1 and 2.
 
    Examples =>
          arithmetic(2, 3, "+") ➞ 5
@@ -13,3 +14,19 @@
    Hint => use if/else conditions to solve this challenge.
 
  */
+
+function arithmetic(num1, num2, sign) {
+  if (sign === "+") {
+    return num1 + num2;
+  } else if (sign === "-") {
+    return num1 - num2;
+  } else if (sign === "*") {
+    return num1 * num2;
+  } else if (sign === "/") {
+    return num1 / num2;
+  } else if (sign === "%") {
+    return num1 % num2;
+  }
+}
+let answers=arithmetic(10,5,"/")
+console.log("PAssed operated answer is",answers)
