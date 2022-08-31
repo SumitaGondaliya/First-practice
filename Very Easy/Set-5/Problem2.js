729ee6daeddbc2637f8ac09fa63b9db29a5a511a
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 19:28:27 +0530

    set5Sol2

--- Very Easy/Set-5/Problem2.js
+++ Very Easy/Set-5/Problem2.js
@@ -1,9 +1,12 @@
 /**
  Write a function that returns true if a year is a leap, otherwise return false.
 
-    A year is a "leap year" if it lasts 366 days, instead of 365 in a typical year. That extra day is added to the end of the shortest month, creating February 29.
+    A year is a "leap year" if it lasts 366 days, instead of 365 in a typical year. 
+    That extra day is added to the end of the shortest month, creating February 29.
 
-    A leap year occurs every four years, and will take place if the year is a multiple of four. The exception to this is a year at the beginning of a century (for example, 1900 or 2000), where the year must be divisible by 400 to be a leap year.
+    A leap year occurs every four years, and will take place if the year is a multiple of four. 
+    The exception to this is a year at the beginning of a century (for example, 1900 or 2000), 
+    where the year must be divisible by 400 to be a leap year.
 
     Look at the examples, and if you need help, look at the resources panel.
 
@@ -17,3 +20,16 @@
     Hint =>
         - Use the modulus operator to check if a year is a multiple of 4.
  */
+
+        function isLeapYear(year)
+         {
+            if (year % 4 === 0) 
+              return true
+
+              else
+
+             return false
+            
+        }
+        let result=isLeapYear(2017)
+        console.log("Is year is Leapyear ?",result)
