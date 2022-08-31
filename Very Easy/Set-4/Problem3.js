a010b8062b80a1b11e712b7776aa1c15cef22cc6
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 17:51:21 +0530

    set4Sol3

--- Very Easy/Set-4/Problem3.js
+++ Very Easy/Set-4/Problem3.js
@@ -1,5 +1,6 @@
 /**
-     Create a function that calculates the area of a rectangle. If the arguments are invalid, your function must return -1.
+     Create a function that calculates the area of a rectangle. If the arguments are invalid, 
+     your function must return -1.
 
      Examples =>
           area(3, 4) ➞ 12
@@ -7,3 +8,10 @@
           area(-1, 5) ➞ -1
           area(0, 2) ➞ -1
  */
+
+function area(l, w) {
+  return l < 1 || w < 1 ? -1 : l * w;
+}
+
+let result = area(0, 2);
+console.log("area of rectangle is ", result);
