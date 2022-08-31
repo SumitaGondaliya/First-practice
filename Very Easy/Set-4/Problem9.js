e214404678c1a50275916b0e2573d48d177be559
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 19:05:04 +0530

    set4Sol9

--- Very Easy/Set-4/Problem9.js
+++ Very Easy/Set-4/Problem9.js
@@ -1,5 +1,6 @@
 /**
-  The ternary operator (sometimes called Conditional Expressions) in JavaScript is an alternative to the if... else... statement.
+  The ternary operator (sometimes called Conditional Expressions) in JavaScript is an alternative to the
+   if... else... statement.
 
    It is written in the format:
       condition ? result_if_true : result_if_false
@@ -13,3 +14,9 @@
       yeah_nope(true) ➞ "yeah"
       yeah_nope(false) ➞ "nope"
  */
+
+function yeah_nope(b) {
+  return b ? "yeah" : "nope";
+}
+let result=yeah_nope(false)
+console.log("result is",result)
