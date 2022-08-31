de8b0b70335c76f317ae0a97ed130e2e227a0d24
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 19:39:59 +0530

    set5Sol3

--- Very Easy/Set-5/Problem3.js
+++ Very Easy/Set-5/Problem3.js
@@ -1,7 +1,9 @@
 /**
  *   ---- String and Number Conversions ----
  * 
-    You need to create two functions to substitute toString() and parseInt(); A function called intToString() that converts integers into strings and a function called stringToInt() that converts strings into integers.
+    You need to create two functions to substitute toString() and parseInt();
+     A function called intToString() that converts integers into strings and 
+     a function called stringToInt() that converts strings into integers.
 
      Examples =>
           intToString(1) ➞ "1"
@@ -11,3 +13,19 @@
      
      
  */
+
+
+          function intToString(n) 
+          {
+            return n.toString()
+          }
+          let result=intToString(100)
+          console.log("Integers to string",result)
+         
+         
+         function stringToInt(n1) 
+         {
+            return parseInt(n1)
+         }
+         let result1=stringToInt("1000")
+         console.log("String to Integer",result1)
