47d6ac6e3a4f601384d24d2e5c3181c826054f16
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-08-31 18:36:39 +0530

    set4Sol5

--- Very Easy/Set-4/Problem5.js
+++ Very Easy/Set-4/Problem5.js
@@ -1,8 +1,17 @@
 /**
-    Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+    Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed.
+     This is calculated by subtracting the number of downvotes from upvotes.
     
     Examples =>
         getVoteCount({ upvotes: 13, downvotes: 0 }) ➞ 13
         getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
         getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
  */
+
+function getVoteCount(v) {
+  let votes = v.upvots - v.downvots;
+  return votes;
+}
+
+let result = getVoteCount({ upvots: 132, downvots: 132 });
+console.log("Votes count should be displayed", result);
