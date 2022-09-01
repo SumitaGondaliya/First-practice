b8508dcc9f6158825e52cf8176c9e107a11aedd3
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-01 23:21:43 +0530

    for6

--- Loops/Problem6.js
+++ Loops/Problem6.js
@@ -1,8 +1,8 @@
-/*Write a JavaScript program to sum the multiples of 3 and 5 under 100.*/
+/*Write a JavaScript program to sum the multiples of  6 and 3 under 100.*/
 
 var sum = 0;
 for (var x = 0; x < 100; x++) {
-  if (x % 3 === 0 || x % 5 === 0) {
+  if (x % 3 === 0 || x % 6 === 0) {
     sum += x;
   }
 }
