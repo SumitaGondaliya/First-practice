0c8741dafff65b24bcc9a1b564189bd969126842
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-03 12:48:26 +0530

    sol7

---
+++ Loops/Problem7.js
@@ -0,0 +1,10 @@
+//Calculate the sum of all the numbers in the following array
+
+
+let numArray = [1, 13, 22, 123, 49];
+let sum = 0;
+for (let i = 0; i < numArray.length; i++) {
+  sum = sum + numArray[i];
+}
+console.log("The sum is: " + sum);
+
