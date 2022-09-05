d9724e44e9917bb7782db81ef4c43c3904ce3bf6
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-05 12:05:01 +0530

    last update

---
+++ Loops/Problem9.js
@@ -0,0 +1,12 @@
+//Given an array of cities and populations, return an array where all populations are rounded to the nearest million.
+
+
+
+let str = '';
+
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+console.log(str);
+
