5aa0b03313f584a076faa3abde14d33bd3afc504
SumitaGondaliya <gondaliyasumita@gmail.com>
2022-09-05 12:04:27 +0530

    do_while

---
+++ Loops/do_while.js
@@ -0,0 +1,10 @@
+
+let text = ""
+let i = 0;
+
+do {
+    text += "The number is " + i;// code pela execute thase
+    i++;
+  }
+  while (i < 10);// loop chalya krse jya sudhi condition true thase ,false thya pcchi break thase.
+  console.log(text)
